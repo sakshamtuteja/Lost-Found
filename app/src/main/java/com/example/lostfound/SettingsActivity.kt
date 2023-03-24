f7af17c8e6867bac4473ff507521bb3a7c474f91
Saksham Tuteja <tutejasaksham@gmail.com>
2023-03-24 12:35:44 +0530

    lost and found firebase authentication

--- app/src/main/java/com/example/lostfound/SettingsActivity.kt
+++ app/src/main/java/com/example/lostfound/SettingsActivity.kt
@@ -4,43 +4,31 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
+import android.widget.TextView
 import android.widget.Toast
 import com.example.lostfound.databinding.ActivitySettingsBinding
 import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.ktx.auth
 import com.google.firebase.firestore.FirebaseFirestore
-
+import com.google.firebase.ktx.Firebase
 
 
 class SettingsActivity : AppCompatActivity() {
-    private lateinit var auth: FirebaseAuth
+    private lateinit var text: TextView
+
+    private lateinit var logout: Button
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_settings)
 
-        val logout = findViewById<Button>(R.id.logoutbutton)
-        logout.setOnClickListener{
-            auth.signOut()
-        Toast.makeText(baseContext,"Logged out", Toast.LENGTH_SHORT)
-        startActivity(intent)
-        }
-
-        val user = auth.currentUser
-        if (user!=null) {
-            val db = FirebaseFirestore.getInstance()
-            db.collection("users").document(user.uid).get()
-                .addOnSuccessListener {document->
-                    if (document !=null){
-                        val name1 = user.displayName
-                        val name2 = user.displayName
-                        val phone = document.get("phone") as String
-
-                    }
-                }
-                .addOnFailureListener { exception->
-                    Toast.makeText(baseContext, "Failed to retrieve user data", Toast.LENGTH_SHORT).show()
-                }
-
+        text = findViewById(R.id.email)
+        logout = findViewById(R.id.logoutbutton)
+        logout.setOnClickListener {
+            Firebase.auth.signOut()
+            val intent = Intent(this, SigninActivity::class.java)
+            startActivity(intent)
+            Toast.makeText(this,"Logout Successful",Toast.LENGTH_LONG).show()
         }
     }
 }
